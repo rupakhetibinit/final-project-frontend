fc24902e6cd8cb3556475e0ed99e11f4d1618012
rupakhetibinit <crusierponder@protonmail.com>
2023-04-30 22:07:57 +0545

    created zustand store to store everything related

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,5 @@
 import { Tweet } from "@/pages/api/gettweets";
+import { TweetV2 } from "twitter-api-v2";
 import { create } from "zustand";
 
 export interface TweetsState {
@@ -6,6 +7,45 @@ export interface TweetsState {
   addTweets: (tweets: Tweet[]) => void;
   wordCloud: { text: string; value: number }[];
   addWordCloud: (words: { text: string; value: number }[]) => void;
+  realTimeQuery: string;
+  addRealTimeQuery: (query: string) => void;
+  realTimeData: { positive: number; negative: number; neutral: number }[];
+  addRealTimeData: (data: {
+    positive: number;
+    negative: number;
+    neutral: number;
+  }) => void;
+  intervalId: number | undefined;
+  setIntervalId: (id: number | undefined) => void;
+  clearRealTimeData: () => void;
+  weeklyTweets: {
+    day: string;
+    tweet: {
+      original: TweetV2;
+      response: Tweet;
+    }[];
+    sentiment: {
+      positive: number;
+      negative: number;
+      neutral: number;
+    };
+  }[];
+  addWeeklyTweets: (
+    weeklyTweets: {
+      day: string;
+      tweet: {
+        original: TweetV2;
+        response: Tweet;
+      }[];
+      sentiment: {
+        positive: number;
+        negative: number;
+        neutral: number;
+      };
+    }[]
+  ) => void;
+  weeklyQuery: string;
+  addWeeklyQuery: (weeklyQuery: string) => void;
 }
 
 const useTweetsStore = create<TweetsState>()((set) => ({
@@ -13,6 +53,26 @@ const useTweetsStore = create<TweetsState>()((set) => ({
   addTweets: (tweets) => set(() => ({ analysisTweets: tweets })),
   wordCloud: [],
   addWordCloud: (words) => set(() => ({ wordCloud: words })),
+  realTimeQuery: "",
+  addRealTimeQuery: (query) => set(() => ({ realTimeQuery: query })),
+  realTimeData: [],
+  addRealTimeData: (data: {
+    positive: number;
+    negative: number;
+    neutral: number;
+  }) => set((state) => ({ realTimeData: [...state.realTimeData, data] })),
+  intervalId: undefined,
+  setIntervalId: (id) => set(() => ({ intervalId: id })),
+  clearRealTimeData: () => set(() => ({ realTimeData: [] })),
+  weeklyTweets: [],
+  addWeeklyTweets: (weeklyTweets) =>
+    set(() => ({ weeklyTweets: weeklyTweets })),
+  weeklyQuery: "",
+  addWeeklyQuery: (weeklyQuery) => set(() => ({ weeklyQuery: weeklyQuery })),
 }));
 
 export { useTweetsStore };
+
+export type TweetV2Required = Required<
+  Pick<TweetV2, "lang" | "id" | "created_at" | "text">
+>;
